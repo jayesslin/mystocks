9232181f7b4337f0cba4435f1c03d5c5d8ead30a
xiaoerya <841686336@qq.com>
2019-03-24 19:52:16 -0500

    complete transaction record

--- app/src/main/java/com/team3/ms/mystocks/view/TransactionRecord.java
+++ app/src/main/java/com/team3/ms/mystocks/view/TransactionRecord.java
@@ -4,11 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.team3.ms.mystocks.R;
 public class TransactionRecord extends AppCompatActivity {
+
+    private String[] data = {"03-22-2019                    FB                          +1000",
+            "03-20-2018                    AMZN                    +300",
+            "03-18-2018                    NFLX                     +500",
+            "03-15-2018                    NKE                       +900",
+            "01-01-2019                    AMZN                    -800",
+            "08-28-2018                    AMZN                    +800",
+            "05-10-2018                    NFLX                     +1000"};
+//    private String[] date = {"03-22-2019", "01-01-2019", "08-28-2018", "05-10-2018"};
+//    private String[] stockID = {"FB", "AMZN", "AMZN", "NFLX"};
+//    private String[] record = {"+1000", "-800", "+800", "+1000"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +43,10 @@ public class TransactionRecord extends AppCompatActivity {
                 startActivity(s1);
             }
         });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (
+                TransactionRecord.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list1);
+        listView.setAdapter(adapter);
     }
 }
