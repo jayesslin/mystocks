2904ad208cdf90dc8ab4a1e26080103a31620bfb
jayesslin <493965578@qq.com>
2019-03-27 18:16:34 -0500

    更新profile 取值

--- app/src/main/java/com/team3/ms/mystocks/view/personalPage.java
+++ app/src/main/java/com/team3/ms/mystocks/view/personalPage.java
@@ -3,11 +3,13 @@ package com.team3.ms.mystocks.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.team3.ms.mystocks.DBmgr.dbmanage;
 import com.team3.ms.mystocks.R;
 import com.team3.ms.mystocks.entity.user;
+import com.team3.ms.mystocks.entity.user_info;
 
 public class personalPage extends AppCompatActivity {
     private TextView Name;
@@ -20,13 +22,24 @@ public class personalPage extends AppCompatActivity {
         dbMgr= new dbmanage(personalPage.this,"MyStocks.db",null,1);
         Name = (TextView)findViewById(R.id.Name);
         EmailAdress = (TextView)findViewById(R.id.EmailAdress);
-        Intent s1 =getIntent();
-        String userName = s1.getStringExtra("id");//获得用户名
-        Name.setText(userName);
+
+        //老方法 intent传值
+ /*       Intent s1 =getIntent();
+        String userName = s1.getStringExtra("id");//获得用户名*/
+            /*  Name.setText(userName);
         //通过用户名获取用户对象
-        user user = dbMgr.getUser(userName);
+        user user = dbMgr.getUser(userName);*/
         //通过对象获取对象邮箱地址
+
+        //单例模式
+        user_info sd= user_info.getInstance();
+        String username = sd.getId();
+        Log.i("*************"," id 是"+ username);
+        Name.setText(username);
+        //通过用户名获取用户对象
+        user user = dbMgr.getUser(username);
         String email_address = user.getEmail();
+        Log.i("*************"," email 是"+ email_address);
         EmailAdress.setText(email_address);
     }
 }
