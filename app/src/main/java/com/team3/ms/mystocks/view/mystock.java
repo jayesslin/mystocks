b85538386f5213a705f54e3da267a3d574ff4f78
xysecurity <41580204+xysecurity@users.noreply.github.com>
2019-04-04 02:17:02 -0500

    加入notify

--- app/src/main/java/com/team3/ms/mystocks/view/mystock.java
+++ app/src/main/java/com/team3/ms/mystocks/view/mystock.java
@@ -1,11 +1,23 @@
 package com.team3.ms.mystocks.view;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -25,19 +37,28 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class mystock extends AppCompatActivity {
     private ListView collect_list;
-    private ImageView backButton;
+    private ImageView backButton,tongzhi,qtongzhi;
     private Context resContext;
     private Handler handler;
-    private List<stocklist> stock_list=new ArrayList<>();
+    public List<stocklist> stock_list=new ArrayList<>();
     private stockAdapter mAdapter=null;
     private dbmanage dbMgr;
+    public static final String TAG = "MyService";
+    private final static String ACTION_NOTIFICATION = "ACTION_NOTIFICATION";
+    boolean flag=true,flag2=true;
+    public Timer timer =new Timer();
+    public int i=0;
+    public String timerstr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_mystock);
         dbMgr= new dbmanage(mystock.this,"MyStocks.db",null,1);
         collect_list = (ListView)findViewById(R.id.collect_list);
@@ -56,12 +77,80 @@ public class mystock extends AppCompatActivity {
         collectcontroller cc = new collectcontroller();
         String[] list = cc.getlist(dbMgr);
         resContext = mystock.this;
+        tongzhi=(ImageView) findViewById(R.id.tongzhi);
+        if (timer.toString().equals("java.util.Timer@de938eb")){
+
+        }
+        else{
+            tongzhi.setImageResource(R.drawable.tongzhi2);
+        }
+
+
         get_list(list);
+
+
         handler =new Handler(){
             public void handleMessage(Message msg){
                 System.out.println(msg.what);
                 if(msg.what == 1){
                     /**/
+                    tongzhi.setOnClickListener(new View.OnClickListener() {
+                        @Override
+
+                        public void onClick(View v) {
+                            Log.i("timer",timer.toString());
+
+
+                            if (flag2){
+
+                                tongzhi.setImageResource(R.drawable.tongzhi);
+
+
+                                        timer.schedule(new TimerTask() {
+
+                                    @Override
+                                    public void run() {
+
+                                        if(flag){
+                                            add(stock_list);
+                                            i++;
+
+                                        }
+                                        else if (!flag){
+                                            loss(stock_list);
+                                            i++;
+                                        }
+                                        flag=!flag;
+
+
+
+                                    }
+                                }, 5000,5000);
+                                new Thread() {
+                                    public void run() {
+                                        Looper.prepare();
+                                        Toast toast=Toast.makeText(getApplicationContext(), "Notify every 5s", Toast.LENGTH_SHORT);
+                                        toast.show();
+                                        Looper.loop();//这种情况下，Runnable对象是运行在子线程中的，可以进行联网操作，但是不能更新UI
+                                    }
+                                }.start();
+
+
+                            }
+                            else{
+                                tongzhi.setImageResource(R.drawable.tongzhi2);
+                                timer.cancel();
+
+
+                            }
+
+
+
+                        }
+                    });
+
+
+
                     /*for (int i = 0 ; i < stock_list.size() ; i++)
                         Log.d("value is" , stock_list.get(i).toString());*/
                     mAdapter=new stockAdapter(stock_list,resContext);
@@ -86,15 +175,20 @@ public class mystock extends AppCompatActivity {
 
 
     }
+    public void showToast(){
+        Toast toast=Toast.makeText(getApplicationContext(), "notify", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
     private void get_list(final String[] sym_list){
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try{ Stocks_provider sp=new Stocks_provider();
                     String try0 = sp.getStock("LEE");
-                    System.out.println("now is "  + try0);
+
                     String result=sp.getRequest5(1);
-                    System.out.println("now is "  + result);
+
                     JSONObject object1=new JSONObject(result);
                     JSONObject object2=object1.getJSONObject("result");
                     JSONArray array1=object2.getJSONArray("data");
@@ -138,4 +232,58 @@ public class mystock extends AppCompatActivity {
             }
         }).start();
     }
+    private void add(List<stocklist> sym_list){
+
+
+        for(int i=0;i<sym_list.size();i++){
+        Log.i("stock",sym_list.get(i).getLimit());
+        float num=Float.parseFloat(sym_list.get(i).getLimit());
+        if (num>0){
+            Log.i("stock",""+num);
+            notify1(sym_list.get(i).getGid(),mystock.this);
+        }
+        }
+
+
+    }
+    private void loss(List<stocklist> sym_list){
+
+
+        for(int i=0;i<sym_list.size();i++){
+            Log.i("stock",sym_list.get(i).getLimit());
+            float num=Float.parseFloat(sym_list.get(i).getLimit());
+            if (num<0){
+                Log.i("stock",""+num);
+                notify1(sym_list.get(i).getGid(),mystock.this);
+            }
+        }
+
+
+    }
+    public void notify1(String number,Context context){
+        Intent acIntent = new Intent(context, mystock.class);
+        NotificationChannel channel = new NotificationChannel("1",
+                "Channel1", NotificationManager.IMPORTANCE_DEFAULT);
+        channel.enableLights(true); //是否在桌面icon右上角展示小红点
+        channel.setLightColor(Color.GREEN); //小红点颜色
+        channel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+
+
+        NotificationManager nm = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.createNotificationChannel(channel);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, acIntent, 0);
+        Notification notify = new NotificationCompat.Builder(this,"1")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("TickerText:" + "You have new message!")
+                .setContentTitle("stockid:"+number)
+                .setContentText("The stock "+number+"limit has changed")
+                .setContentIntent(pi).build();
+        notify.flags |= Notification.FLAG_AUTO_CANCEL; // FLAG_AUTO_CANCEL表明当通知被用户点击时，通知将被清除。
+        nm.notify(1, notify);
+    }
+
+
+
+
 }
+
